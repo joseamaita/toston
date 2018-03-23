58719b16cf46e40d0bc31c6d79a02a3aa3a5de41
José Alberto Maita Fariñas <jose_a_maita@hotmail.com>
2018-03-23 15:58:17 -0400

    Update module package
      This adds:
    
      - Console script updated (toston/cli.py)
      - Test for command line interface (tests/test_toston.py)

--- tests/test_toston.py
+++ tests/test_toston.py
@@ -30,9 +30,10 @@ def test_content(response):
 def test_command_line_interface():
     """Test the CLI."""
     runner = CliRunner()
-    result = runner.invoke(cli.main)
+    result = runner.invoke(cli.main, ['8'])
     assert result.exit_code == 0
-    assert 'toston.cli.main' in result.output
+    assert '' in result.output
+    #assert 'toston.cli.main' in result.output
     help_result = runner.invoke(cli.main, ['--help'])
     assert help_result.exit_code == 0
     assert '--help  Show this message and exit.' in help_result.output
--- toston/cli.py
+++ toston/cli.py
@@ -1,16 +1,17 @@
 # -*- coding: utf-8 -*-
 
 """Console script for toston."""
+
 import sys
 import click
+from toston.toston import seckey
 
 
 @click.command()
-def main(args=None):
+@click.argument('size', type=int)
+def main(size):
     """Console script for toston."""
-    click.echo("Replace this message by putting your code into "
-               "toston.cli.main")
-    click.echo("See click documentation at http://click.pocoo.org/")
+    seckey(size)
     return 0
 
 
